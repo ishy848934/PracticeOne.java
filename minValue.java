16616a20613742fca802dfed919ab7334d7229d1
ishy848934 <ishamm2828@gmail.com>
2026-01-29 16:54:16 -0700

    Change max to min and print minimum value

--- minValue.java
+++ minValue.java
@@ -1,17 +1,17 @@
 public class minValue{
     public static void main(String[]args){
         int [] nums = {-2,0,5,7,9};
-        int max =nums[0];
+        int min =nums[0];
 
 
     for (int i = 0; i < nums.length; i++) {
             
-            if (nums[i] > max) {
-                max = nums[i];
+            if (nums[i] < min) {
+                min = nums[i];
             }
         }
 
-        System.out.println(max);
+        System.out.println(min);
     }
 }
     
